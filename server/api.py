5a37f9536bfe42d9421d7107a448991dba9dcd46
Sujaychinnam <sujaychinnam000@gmail.com>
2026-03-10 01:55:36 +0530

    added web dashboard

--- server/api.py
+++ server/api.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+
 import shutil
+import os
 
 from backend.crypto_engine import encrypt_file
 from backend.cloud_storage import upload_file
@@ -7,31 +12,47 @@ from backend.blockchain import Blockchain
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="server/templates")
+
 blockchain = Blockchain()
 
 
-@app.get("/")
-def root():
-    return {"message":"Secure Cloud API running"}
+# =========================
+# Web Dashboard
+# =========================
+
+@app.get("/", response_class=HTMLResponse)
+def dashboard(request: Request):
+
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "chain": blockchain.chain}
+    )
 
 
-@app.post("/upload/")
-async def upload(file: UploadFile):
+# =========================
+# Upload API
+# =========================
+
+@app.post("/upload")
+async def upload_file_api(file: UploadFile = File(...)):
+
+    os.makedirs("storage", exist_ok=True)
 
     path = f"storage/{file.filename}"
 
-    with open(path,"wb") as buffer:
+    with open(path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    key, encrypted_file = encrypt_file(path)
+    key, encrypted = encrypt_file(path)
 
-    cloud_name = upload_file(encrypted_file)
+    cloud_name = upload_file(encrypted)
 
     blockchain.add_block(
         cloud_name,
         key,
-        "api_user",
+        "web_user",
         "receiver"
     )
 
-    return {"file":cloud_name}
+    return {"status": "uploaded", "file": cloud_name}
